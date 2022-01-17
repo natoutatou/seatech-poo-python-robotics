a74382c3dc0b669fa6324b454bebd92e46e9f620
natoutatou <ceciestmonadresse07@gmail.com>
2022-01-17 10:09:10 +0100

    Ajout meth charge

--- Exo1.py
+++ Exo1.py
@@ -9,19 +9,38 @@ class Robot():
     """
       Give your best code here ( •̀ ω •́ )✧
     """
-
-
-    def __init__(self, name)
+    	
+    def __init__(self, name=None):
+        if name:
+            self.__name = name
+        global ROBOT_COUNT
+        ROBOT_COUNT += 1
 
 
     def __init__(self, power=False):  
       if (power == True):
         self.__states = 'Running'
         self.__power = True
-      else
+      else:
         self.__states = 'Sutdown'
 
-    def __init__(self, states, __battery_level, )
 
+def charge (self, __battery_level):
+  if (__battery_level>80):
+    print("Robot chargé à au moins 80%, chargez plus tard")
+  else:
+    charge_length = 101 - __battery_level
+  for i in range(charge_length):
+    print(__battery_level)
+    __battery_level += 1
+  print("Le Robot est chargé")
+
+#def move (self, __current_speed):
 
 r = Robot(power=True)
+r.__name = "Roger"
+print(r.__name)
+charge(r, 7)
+
+
+
